729e4b2f5b6e336a00024c2aa5fda2d3e2e116b2
PROF ROMULO <rjlpena@gmail.com>
2023-10-10 07:57:57 -0300

    const lista de teclas

--- main.js
+++ main.js
@@ -7,3 +7,6 @@ function tocaSomClap(){
     document.querySelector('#som_tecla_clap').play();
 }
 document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+listaDeTeclas[0].onclick = tocaSomPom;
