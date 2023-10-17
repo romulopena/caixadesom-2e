4d2f8066ebe89be137f16b3c7603d8d94d67529e
PROF ROMULO <rjlpena@gmail.com>
2023-10-17 07:56:01 -0300

    while

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
+//criando/declarando a função tocaSomPom
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
+// chamando/invocando a função tocaSomPom
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play();
-}
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
-
+//criando a referência constante listaDeTeclas
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//atribuindo o clique no botão "0" que é o 1º item da lista de teclas.
 listaDeTeclas[0].onclick = tocaSomPom;
+//criando referência variável contador, atribuindo o valor inicial como "0"
+let contador = 0;
+//Criando o código de repetição WHILE
+while(){
+    
+}
