b2939e7a8f9bb820a6f0d47b9402404c2316b9bc
PROF ROMULO <rjlpena@gmail.com>
2023-11-14 07:51:49 -0300

    onkeydown

--- main.js
+++ main.js
@@ -5,12 +5,15 @@ function tocaSom(idElementoAudio){
 //criando a referência constante listaDeTeclas
 const listaDeTeclas = document.querySelectorAll('.tecla');
 //Criando o código de repetição WHILE e mudando para FOR
-//enquanto o contador for menos que o comprimento da lista, tocará o som.
+//enquanto o contador < comprimento da lista, tocará o som.
 for(let contador = 0;contador < listaDeTeclas.length;contador++){
     const tecla = listaDeTeclas[contador];
     const efeito = tecla.classList[1]; //chama a classe do indice 1 do som
     const idAudio = `#som_${efeito}`; //junção com template string
     tecla.onclick = function(){ //função anônima para tocaSom
         tocaSom(idAudio);
+    }
+    tecla.onkeydown = function(){
+        tecla.classList.add('ativa');
     }
 }
